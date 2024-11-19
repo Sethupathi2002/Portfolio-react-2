abb2fd055688732ec75b433109de845723541644
Sethupathi <116177552+Sethupathi2002@users.noreply.github.com>
2024-11-19 19:51:36 +0530

    Icon Added

--- src/components/04_Projects/Projects.jsx
+++ src/components/04_Projects/Projects.jsx
@@ -1,17 +1,18 @@
-import './Projects.css'
+import './Projects.css';
+import homePageImage from '../../assets/home_page_image.png';
 
 function Projects() {
     return (
         <div className='projects-container'>
             <h2>Projects</h2>
             <div className='project-content'>
-                <center>
-                    <p>Haven't done anything good enough to showcase</p>
-                </center>
+                <a href="https://baratie07.netlify.app/" target='_blank' rel="noreferrer">
+                    <img src={homePageImage} className='home-page-image' alt="Placeholder" />
+                </a>
+                <h3 className='project-heading-title'>Food Delivery Application</h3>
             </div>
-
         </div>
     )
 }
 
-export default Projects
+export default Projects;
